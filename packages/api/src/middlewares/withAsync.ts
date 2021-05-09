e095f919afab7a1acbcb1516cc747162cc634226
survi <szymonhrabia60@gmail.com>
2021-05-09 18:58:33 +0200

    Add try/catch for user.controller to catch validations errors

--- packages/api/src/middlewares/withAsync.ts
+++ packages/api/src/middlewares/withAsync.ts
@@ -8,12 +8,6 @@ export const withAsync = (routeCallback: RequestHandler) => async (
   try {
     await routeCallback(req, res, next);
   } catch (error) {
-    if (error.status) {
-      return res.status(error.status).json({ status: error.status, message: error.message });
-    }
-    if (error.name === 'ValidationError') {
-      return res.status(422).json({ status: 422, message: error.errors });
-    }
     return res.status(500).json({ status: 500, message: error });
   }
 };
--- packages/api/src/modules/user/user.controller.ts
+++ packages/api/src/modules/user/user.controller.ts
@@ -1,31 +1,43 @@
-import type { RequestHandler } from 'express';
+import type { Request, Response } from 'express';
 import { hash } from 'bcrypt';
 import { userRegisterSchema } from './user.schema';
 import { BCRYPT_SALT_ROUNDS } from '../../constants/constants';
 import { findUserBy } from '../../utility/findBy';
 import { saveUserToDatabase } from './user.service';
-
-export const createUser: RequestHandler = async (req, res) => {
-  const { username, password, email } = await userRegisterSchema.validate(req.body);
-  const user = await findUserBy({
-    OR: [
-      {
-        username,
-      },
-      {
-        email,
-      },
-    ],
+import { config } from 'dotenv';
+if (process.env.NODE_ENV === 'TEST') {
+  config({
+    path: '../.env.test',
   });
-  if (user) {
-    throw { status: 409, message: 'User already exists' };
-  }
+} else {
+  config();
+}
 
-  const hashedPassword = await hash(password, BCRYPT_SALT_ROUNDS);
-  const savedUser = await saveUserToDatabase({ username, password: hashedPassword, email });
-  if (savedUser) {
-    return res
-      .status(201)
-      .json({ status: 201, message: 'User successfully created! You can log in now.' });
+export const createUser = async (req: Request, res: Response) => {
+  try {
+    const { username, password, email } = await userRegisterSchema.validate(req.body);
+    const user = await findUserBy({
+      OR: [
+        {
+          username,
+        },
+        {
+          email,
+        },
+      ],
+    });
+    if (user) {
+      return res.status(409).json({ message: 'User already exists' });
+    }
+
+    const hashedPassword = await hash(password, BCRYPT_SALT_ROUNDS);
+    const savedUser = await saveUserToDatabase({ username, password: hashedPassword, email });
+    if (savedUser) {
+      return res
+        .status(201)
+        .json({ status: 201, message: 'User successfully created! You can log in now.' });
+    }
+  } catch (e) {
+    return res.status(422).json({ status: 422, message: e.errors });
   }
 };
