555a7216441df81bea8574dba3e8f7dd7ce6efc8
survi <szymonhrabia60@gmail.com>
2021-06-03 11:22:52 +0200

    Add route for session endpoint

--- packages/api/src/routes.ts
+++ packages/api/src/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { userRouter } from './modules/auth/auth.router';
+import { sessionRouter } from './modules/session/session.router';
 
 export const appRouter = Router();
 
 appRouter.use('/users', userRouter);
+appRouter.use('/session', sessionRouter);
